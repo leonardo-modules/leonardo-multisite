2d80833ae7acc6d6710f955c7bfab020da4d1b81
Michael Kutý <6du1ro.n@gmail.com>
2016-01-02 14:16:50 +0100

    Add configurable SESSION_COOKIE_DOMAIN for sharing cookie between subdomains.

--- leonardo_multisite/__init__.py
+++ leonardo_multisite/__init__.py
@@ -11,6 +11,11 @@ LEONARDO_MIDDLEWARES = ['leonardo_multisite.middleware.MultiSiteMiddleware']
 LEONARDO_CONFIG = {
     'MULTISITE_ENABLED': (False, _(
         'Enable multi site request processing')),
+    'SESSION_COOKIE_DOMAIN': ('', _(
+        '''If you set your session cookie domain to start with
+        a "." character it will let you handle wildcard sub-domains
+        and share a session cookie (login session) across multiple
+        subdomains.''')),
 }
 
 
